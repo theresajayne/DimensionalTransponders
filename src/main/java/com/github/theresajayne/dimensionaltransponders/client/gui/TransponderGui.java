f9c03447e1b080b305b02c6c783e246d3c2ec2d1
Theresa Forster <theresajayne@minuet-uk.com>
2015-12-13 18:51:26 +0000

    Extra commit after a while - cannot remember what it is all about

---
+++ src/main/java/com/github/theresajayne/dimensionaltransponders/client/gui/TransponderGui.java
@@ -0,0 +1,9 @@
+package com.github.theresajayne.dimensionaltransponders.client.gui;
+
+import net.minecraft.client.gui.GuiScreen;
+
+/**
+ * Created by Theresa on 24/10/2015.
+ */
+public class TransponderGui extends GuiScreen {
+}
